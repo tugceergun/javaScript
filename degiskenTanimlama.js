bcbe5dc7839761d15c60ea0d9b5ac29688432348
Tuğçe Ergün <tugceergun000@gmail.com>
2023-02-25 20:58:31 +0300

    js degisken tanımlama.

---
+++ degiskenTanimlama.js
@@ -0,0 +1,30 @@
+//var
+var serverName = " kodluyoruz.org"
+console.log(serverName)
+
+serverName = "https://kodluyoruz.org"
+console.log(serverName)
+
+let password = "1234";
+console.log(password)
+
+let fullName = "Tuğçe Ergün";
+
+fullName="Lorem Ipsum"
+console.log(fullName);
+
+fullName + "Yeni eklenen bilgi" //eklemedi
+console.log(fullName + " Test bilgisi");
+
+fullName=fullName+ " Yeni bilgi"
+fullName= "2. bilgi : " + fullName
+
+fullName="sifirladik"
+fullName += " ve yeni bilgi eklendi."
+console.log(fullName);
+
+const SERVER_PASSWORD = "osdfghfkj221"
+console.log(SERVER_PASSWORD);
+
+//SERVER_PASSWORD="1234" //const degismez.
+//console.log(SERVER_PASSWORD) 
