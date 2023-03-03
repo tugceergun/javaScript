9befc1a9e5be9e5df7d3831238db3ffaa9afda43
Tuğçe Ergün <tugceergun000@gmail.com>
2023-03-03 21:05:17 +0300

    Kosul yapisi.

--- kosulYapisi.js
+++ kosulYapisi.js
@@ -32,4 +32,27 @@ switch(hava) {
   default:
       console.log("Bilinmeyen hava durumu:" , hava);
 }
+
+//-----------
+let islem = function(a, b, operator) {
+    switch(operator) {
+        case 'topla':
+            return a + b;
+        break;
+        case 'cikar':
+            return a - b;
+        break;
+        case 'carp':
+            return a * b;
+        break;
+        case 'bol':
+            return a / b;
+        break;
+        default:
+            return 'Tanimlanmamis islem';
+        break;
+    }
+};
+console.log(islem(23, 14, 'topla'));  // 37
+console.log(islem(10, 3, 'mod'));     // Tanimlanmamis islem
 */
